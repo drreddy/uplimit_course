ecd5c2f8fa0d428c98f3b3148cf36aa9ccb40a76
D. Rajeev. Reddy <rajeev.reddy.d@gmail.com>
2023-08-21 23:36:02 +0530

    Updated guest details container

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -38,22 +38,27 @@ def main():
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
+        # col3, col4 = st.columns([3, 7])
 
-        with col3:
+        # with col3:
+        #     st.subheader("Podcast Guest")
+        #     st.write(podcast_info['podcast_guest']['name'])
+
+        # with col4:
+        #     st.subheader("Podcast Guest Details")
+        #     st.write(podcast_info["podcast_guest"]['summary'])
+
+        with st.container():
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
+            st.write(podcast_info['podcast_guest'])
 
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        with st.container():
+            # Display the five key moments
+            st.subheader("Key Moments")
+            key_moments = podcast_info['podcast_highlights']
+            for moment in key_moments.split('\n'):
+                st.markdown(
+                    f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
     # User Input box
     st.sidebar.subheader("Add and Process New Podcast Feed")
@@ -86,22 +91,27 @@ def main():
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
+        # col3, col4 = st.columns([3, 7])
 
-        with col3:
+        # with col3:
+        #     st.subheader("Podcast Guest")
+        #     st.write(podcast_info['podcast_guest'])
+
+        # with col4:
+        #     st.subheader("Podcast Guest Details")
+        #     st.write(podcast_info["podcast_guest"]['summary'])
+
+        with st.container():
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
+            st.write(podcast_info['podcast_guest'])
 
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        with st.container():
+            # Display the five key moments
+            st.subheader("Key Moments")
+            key_moments = podcast_info['podcast_highlights']
+            for moment in key_moments.split('\n'):
+                st.markdown(
+                    f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
